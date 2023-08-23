718b30c0f254eb8aaad9fd9b67e7756b8896a04f
Sachin9673 <chavansachin9673gmail.com>
2023-08-23 23:57:27 +0530

    day2_added

--- Frontend/my-react-app/src/App.js
+++ Frontend/my-react-app/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
