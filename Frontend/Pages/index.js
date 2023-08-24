35fb95e3b848007138f05acadb946ebdab2f17e9
Sachin9673 <chavansachin9673gmail.com>
2023-08-24 22:34:34 +0530

    day3_added

---
+++ Frontend/Pages/index.js
@@ -0,0 +1,32 @@
+$(function () {
+    $('#navbar').load('navbar.html');
+    $('#footer').load('footer.html');
+  })
+  
+  
+  const swiper = new Swiper('.swiper', {
+      autoplay:{
+          delay:3000,
+          disableOnInteraction:false,
+      },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+  let hamburger=document.querySelector(".hamburger-menu")
+  function handleHamburger(){
+    console.log("hello")
+    hamburger.classList.toggle("activate")
+  
+  }
